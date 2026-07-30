916a1f22f0048106dd16e6a66c80d8a5f828acbd
Albert Mejia <mejia.albert@gmail.com>
2026-07-30 06:35:58 +0000

    Switch Put Your Hands download to high-quality MP3 and remove original WAV

--- src/linksData.js
+++ src/linksData.js
@@ -92,7 +92,7 @@ export const downloadsData = [
   {
     slug: "put-your-hands-where-my-eyes-could-see",
     title: "Put Your Hands Where My Eyes Could See",
-    url: "/downloads/summers%20over%20-%20Put%20Your%20Hands%20Where%20My%20Eyes%20Could%20See%20remix.wav",
+    url: "/downloads/summers%20over%20-%20Put%20Your%20Hands%20Where%20My%20Eyes%20Could%20See%20remix.mp3",
     previewEmbedUrl: '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2371300358%3Fsecret_token%3Ds-22NNIBXUn7f&color=%235cbf6f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/summers-over" title="summers over" target="_blank" style="color: #cccccc; text-decoration: none;">summers over</a> · <a href="https://soundcloud.com/summers-over/putyourhands/s-22NNIBXUn7f" title="Put Your Hands Where My Eyes Could See [remix]" target="_blank" style="color: #cccccc; text-decoration: none;">Put Your Hands Where My Eyes Could See [remix]</a></div>',
   },
   {
